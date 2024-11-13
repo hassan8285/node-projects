a9c5127e89e82692a823ae53bb6ad02c93cb225d
hassan8285 <hassanshafiquekhan@icloud.com>
2024-11-13 13:13:31 +0500

    Add files via upload

---
+++ connectmongo.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require('mongodb')
+const url = 'mongodb://localhost:27017';
+let database = 'admin'
+const client = new MongoClient(url);
+
+async function dbconnect() {
+    let result = await client.connect();
+     db = result.db(database)
+    return db.collection('products');}
+    module.exports=dbconnect;
